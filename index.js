6b35c1165ac22419f4f26de44b952c2cc303ee66
Sone <sthaya@gmail.com>
2020-06-04 13:43:10 -0700

    changed start script

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
+const express = require('express')
+
 const server = require('./server');
 const helmet = require('helmet')
+
+server.use(helmet())
 
 server.get('/', function (req, res) {
   res.status(200).json({greeting: process.env.GREETING})
